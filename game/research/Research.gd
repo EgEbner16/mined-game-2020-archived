1738951eb70e92adc8126efa28e8b3d731606343
Nathan <drugen@gmail.com>
2020-08-23 18:37:31 -0600

    Research update

--- game/research/Research.gd
+++ game/research/Research.gd
@@ -2,10 +2,38 @@ extends Object
 
 class_name Research
 
-var complete = false
 
-func _init():
-	pass
+var research_name := ''
+var tier := 0
+var progress := 0.0
+var progress_required := 0.0
+var in_progress := false
+var is_complete := false
+var affect := 0.0
+
+
+func _init(research_name: String, tier: int, progress_required: float, affect: float):
+	self.research_name = research_name
+	self.tier = tier
+	self.affect = affect
+	self.progress_required = progress_required
+
 
 func _ready():
 	pass
+
+
+func start_progress() -> void:
+	self.in_progress = true
+
+
+func add_progress(amount: float) -> void:
+	self.progress += amount
+	if self.progress >= self.progress_required:
+		self.progress = self.progress_required
+		self.is_complete = true
+
+
+func get_progress_percentage() -> float:
+	var percentage := (self.progress / self.progress_required) * 100
+	return percentage
--- game/research/ResearchManager.gd
+++ game/research/ResearchManager.gd
@@ -1,6 +1,11 @@
 extends Node
 
+
 class_name ResearchManager
+
+
+onready var research_interface := get_node('/root/Game/InterfaceManager/ResearchInterface') 
+
 
 func _ready():
 	pass
